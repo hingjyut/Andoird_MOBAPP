aeb743157c78c784c2f126eb3a0beebe2e19fb99
x18zhan2 <hingjyutcheung@hotmail.com>
2021-03-25 21:19:45 +0100

    modified

--- app/src/main/java/fr/imt_atlantique/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/fr/imt_atlantique/example/myfirstapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
