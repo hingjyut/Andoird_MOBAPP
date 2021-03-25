5f98f6c6b2c0361481768d3ca6f5f36ac3a4df95
x18zhan2 <hingjyutcheung@hotmail.com>
2021-03-25 18:50:15 +0100

    add internal and external storage codes to contact

--- app/src/main/java/fr/imt_atlantique/example/myfirstapplication/MainActivity.java
+++ app/src/main/java/fr/imt_atlantique/example/myfirstapplication/MainActivity.java
@@ -1,12 +1,18 @@
 package fr.imt_atlantique.example.myfirstapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.text.InputType;
 import android.util.Log;
 import android.view.Gravity;
@@ -15,13 +21,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -51,6 +61,19 @@ public class MainActivity extends AppCompatActivity {
     public static final int SURNAME_CODE = 6;
     public static final int SURNAME_ANSWER_CODE = 60;
 
+    private Button cameraBtn;       // A button for turning camera on
+    private ImageView imageView;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int SAVE_PHOTO_INTO_GALLERY = 2;
+    private static final String TAG = "MainActivity";
+
+
+    private String currentPhotoPath;
+    private Uri currentPhotoUri;
+    private File currentPhotoFile;
+    private String currentPhotoName;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,6 +90,23 @@ public class MainActivity extends AppCompatActivity {
         cityEt = findViewById(R.id.city_et);
         validBt = findViewById(R.id.validate);
         surnameEt.setFocusable(false);
+
+        setContentView(R.layout.activity_main);
+        cameraBtn = findViewById(R.id.camera_btn);
+        imageView = findViewById(R.id.image_view);
+        // Button onclick function
+        cameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Log.i("Start camera","onCreate mode");
+                    startCamera();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
         SharedPreferences sharedPreferences = getSharedPreferences(getString(R.string.first_app_data), Context.MODE_PRIVATE);
         editor = sharedPreferences.edit();
 //        editor.clear(); // clear preference file
@@ -80,6 +120,188 @@ public class MainActivity extends AppCompatActivity {
         }
         Log.i("Lifecycle", "onCreate method");
     }
+
+
+    /**
+     * TP final: storage
+     */
+
+    public void startCamera() throws IOException {
+
+//        //Internal
+//        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+        // External
+        // start a new implicit intent
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            try {
+                currentPhotoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (currentPhotoFile != null) {
+                currentPhotoUri = FileProvider.getUriForFile(this,
+                        "fr.imt_atlantique.example.myfirstapplication.provider",
+                        currentPhotoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, currentPhotoUri);   // Define photo's storage location
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);       // Starts to take photo
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case BIRTHDAY_CODE:
+                if (resultCode == DATE_ANSWER_CODE) {
+                    String message = data.getStringExtra("birthday_data");
+                    birthdayEt.setText(message);
+                }
+                break;
+            case SURNAME_CODE:
+                if (resultCode == SURNAME_ANSWER_CODE) {
+                    surnameEt.setFocusable(true);
+                    String lastName = data.getStringExtra("modified last name");
+                    surnameEt.setText(lastName);
+                    surnameEt.setFocusable(false);
+                }
+                break;
+            //internal
+//            case REQUEST_IMAGE_CAPTURE:
+//                if (resultCode==Activity.RESULT_OK){
+//                    Log.d(TAG, "requestCode: " + requestCode + "  resultCode: " + resultCode + "  data：" + data);
+//                    Bitmap bitmap = null;
+//                    try {
+//                        Bundle extras = data.getExtras();
+//                        bitmap = (Bitmap) extras.get("data");
+//                        File thumbnailFile = createFile(getFilesDir(), "thumbnails");
+//                        FileOutputStream fos = new FileOutputStream(thumbnailFile);
+//                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+//                        fos.flush();
+//                        fos.close();
+//                        imageView.setImageBitmap(bitmap);   // shows photo on the screen
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//                break;
+
+            // external
+            case REQUEST_IMAGE_CAPTURE:
+                if (resultCode==Activity.RESULT_OK){
+                    Log.d(TAG, "requestCode: " + requestCode + "  resultCode: " + resultCode + "  data：" + data);
+                    Bitmap bitmap = null;
+                    try {
+                        // Once user finished taking photo, the photo has been stored in "currentPhotoUri", so now gets the photo back
+                        bitmap = MediaStore.Images.Media.getBitmap(
+                                this.getContentResolver(),
+                                currentPhotoUri
+                        );
+
+
+                        //new lines
+                        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, outputStream);//压缩50%
+                        ByteArrayInputStream isBm = new ByteArrayInputStream(outputStream.toByteArray());//把压缩后的数据存放到ByteArrayInputStream中
+                        bitmap = BitmapFactory.decodeStream(isBm, null, null);//把ByteArrayInputStream数据生成图片
+
+                        System.out.println(bitmap.getHeight());
+                        System.out.println(bitmap.getWidth());
+
+                        imageView.setImageBitmap(bitmap);   // shows photo on the screen
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                galleryAddPic();
+                break;
+
+        }
+    }
+
+//    // 压缩文件大小但图片像素不变
+//    private Bitmap compressImage(Bitmap image) {
+//        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+//        image.compress(Bitmap.CompressFormat.JPEG, 50, outputStream);//质量压缩方法，这里100表示不压缩，把压缩后的数据存放到baos中
+//        int options = 50;
+//        while (outputStream.toByteArray().length / 1024>500) {  //循环判断如果压缩后图片是否大于500kb,大于继续压缩
+//            outputStream.reset(); //重置baos即清空baos
+//            image.compress(Bitmap.CompressFormat.JPEG, options, outputStream);//这里压缩options%，把压缩后的数据存放到baos中
+//            options -= 10; //每次都减少10
+//        }
+//        ByteArrayInputStream isBm = new ByteArrayInputStream(outputStream.toByteArray());//把压缩后的数据baos存放到ByteArrayInputStream中
+//        Bitmap bitmap = BitmapFactory.decodeStream(isBm, null, null);//把ByteArrayInputStream数据生成图片
+//        return bitmap;
+//    }
+
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        currentPhotoName = imageFileName;
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        System.out.println(storageDir.toString()+"storage dir");
+        if (!(storageDir.exists())) {
+            storageDir.mkdir();
+        }
+        System.out.println(storageDir.toString());
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+        public static File createFile(File storageDir, String folderName) throws IOException {
+        // Create a file name
+        String fileName = createUniqueFileName();
+        // Use an existing directory or create it if necessary
+        File picturesDir = new File(storageDir, folderName);
+        if (!(picturesDir.exists())) {
+            picturesDir.mkdir();
+        }
+
+        // Create the name of the file with suffix .jpg
+        File pathToFile = File.createTempFile(
+                fileName,
+                ".jpg",
+                picturesDir
+        );
+
+        System.out.println(pathToFile.toString());
+        return pathToFile;
+    }
+
+    private static String createUniqueFileName() throws IOException {
+        // Create a unique file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String fileName = "JPEG_" + timeStamp + "_";
+        return fileName;
+    }
+
+
 
     /**
      * Récupération des informations saisies dans une activité lancée par intent
@@ -263,6 +485,7 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+
     }
 
     /**
@@ -273,29 +496,6 @@ public class MainActivity extends AppCompatActivity {
     public void go2DateActivity(View view) {
         Intent intent = new Intent(Intent.ACTION_PICK);
         startActivityForResult(intent, BIRTHDAY_CODE);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            case BIRTHDAY_CODE:
-                if (resultCode == DATE_ANSWER_CODE) {
-                    String message = data.getStringExtra("birthday_data");
-                    birthdayEt.setText(message);
-                }
-                break;
-            case SURNAME_CODE:
-                if (resultCode == SURNAME_ANSWER_CODE) {
-                    surnameEt.setFocusable(true);
-                    String lastName = data.getStringExtra("modified last name");
-                    surnameEt.setText(lastName);
-                    surnameEt.setFocusable(false);
-                }
-                break;
-
-        }
     }
 
     /**
